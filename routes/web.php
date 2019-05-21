441f17318c83148d9c6e360e6f1a42b0b0668dcd
Deryhlazov Oleksandr <oleksandr.deryhlazov75@gmail.com>
2019-05-21 22:41:09 +0300

    admin master page setup

--- routes/web.php
+++ routes/web.php
@@ -22,4 +22,12 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+Route::get('/admin' , function() {
+    return view('admin.index');
+});
+
+
 Route::resource('admin/users', 'AdminUsersController');
+Route::resource('admin/posts', 'AdminPostsController');
+Route::resource('admin/categories', 'AdminCategoriesController');
+Route::resource('admin/media', 'AdminMediaController');
