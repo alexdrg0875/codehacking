e8ad0ce594d1808b9aaab03b24643388bda5a415
Deryhlazov Oleksandr <oleksandr.deryhlazov75@gmail.com>
2019-05-22 01:48:42 +0300

    create users page setup

---
+++ resources/views/admin/users/create.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts/admin')
+
+@section('content')
+    <h1>Create users</h1>
+
+    {!! Form::open(['method'=>'POST', 'action'=>'AdminUsersController@store', 'files'=>true]) !!}
+
+        <div class="form-group">
+            {!! Form::label('name', 'Name:') !!}
+            {!! Form::text('name', null, ['class'=>'form-control']) !!}
+        </div>
+        <div class="form-group">
+            {!! Form::label('email', 'E-mail:') !!}
+            {!! Form::email('email', null, ['class'=>'form-control']) !!}
+        </div>
+        <div class="form-group">
+            {!! Form::label('role_id', 'Role:') !!}
+            {!! Form::select('role_id',['' => 'Choose options'] + $roles, null, ['class'=>'form-control']) !!}
+        </div>
+        <div class="form-group">
+            {!! Form::label('is_active', 'Status:') !!}
+            {!! Form::select('is_active', array(1 => 'Active', 0 => 'Not active'), 0, ['class'=>'form-control']) !!}
+        </div>
+        <div class="form-group">
+            {!! Form::label('password', 'Password:') !!}
+            {!! Form::password('password', ['class'=>'form-control']) !!}
+        </div>
+             {{--{!! Form::hidden('user_id', '0', ['class'=>'form-control']) !!}--}}
+
+         {{--<div class="form-group">--}}
+             {{--{!! Form::file('file', ['class'=>'form-control']) !!}--}}
+         {{--</div>--}}
+
+         <div class="form-group">
+
+             {!! Form::submit('Create User', ['class'=>'btn btn-primary']) !!}
+
+         </div>
+
+    {!! Form::close() !!}
+
+@include('includes.form_error')
+
+@endsection
+
+@section('footer')
+@endsection
